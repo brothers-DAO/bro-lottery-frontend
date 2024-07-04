35aff27f017a7bb485894ee97037e0bbb362c83c
Floor de Best <debestfloor@gmail.com>
2024-07-04 15:44:35 +0200

    scale down for large screen

--- src/components/BuyTicket.vue
+++ src/components/BuyTicket.vue
@@ -15,30 +15,34 @@ const orderStore = useOrderStore()
 </script>
 
 <template>
-  <section class="w-full p-[16px] rounded-lg bg-white space-y-[16px] text-white text-[14px]">
+  <section
+    class="flex flex-col items-center w-full p-[16px] rounded-lg bg-white space-y-[16px] text-white text-[14px]"
+  >
     <CountDown
       v-if="lottery.currentRound"
       :timeStart="Date.now()"
       :timeEnd="lottery.currentRound.endTime.getTime()"
     />
-    <AvailableTickets
-      :tickets="lottery.currentRound.tickets"
-      :limit="lottery.currentRound.limit"
-      v-if="lottery.currentRound"
-    />
+    <div class="w-full flex flex-col max-w-[500px] space-y-[16px]">
+      <AvailableTickets
+        :tickets="lottery.currentRound.tickets"
+        :limit="lottery.currentRound.limit"
+        v-if="lottery.currentRound"
+      />
 
-    <TokenSelector />
-    <TicketPrice />
-    <TicketSelector v-if="orderStore.order?.isLoading != undefined" />
-    <div v-if="orderStore.order?.isLoading != undefined || orderStore.order?.isLoading">
-      <div v-if="account.account.isConnected">
-        <CustomButton
-          :title="orderStore.order!.tickets > 1 ? 'Buy Tickets' : 'Buy Ticket'"
-          @click="orderStore.buyTickets()"
-        />
-      </div>
-      <div v-else @click="account.toggleModal()">
-        <CustomButton :title="'Connect Wallet'" />
+      <TokenSelector />
+      <TicketPrice />
+      <TicketSelector v-if="orderStore.order?.isLoading != undefined" />
+      <div v-if="orderStore.order?.isLoading != undefined || orderStore.order?.isLoading">
+        <div v-if="account.account.isConnected">
+          <CustomButton
+            :title="orderStore.order!.tickets > 1 ? 'Buy Tickets' : 'Buy Ticket'"
+            @click="orderStore.buyTickets()"
+          />
+        </div>
+        <div v-else @click="account.toggleModal()">
+          <CustomButton :title="'Connect Wallet'" />
+        </div>
       </div>
     </div>
   </section>
