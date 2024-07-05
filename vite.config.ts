8057b3c52b11352d5f4eee27cf20aadb59e6acd0
Floor de Best <debestfloor@gmail.com>
2024-07-05 13:59:00 +0200

    vite base fix

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,6 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/bro_lottery/',
   plugins: [vue(), vueJsx()],
   server: {
     port: 5500
